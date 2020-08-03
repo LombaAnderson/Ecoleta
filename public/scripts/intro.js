383e335fa1eccd930c6be41095ccaccd399241b6
Anderson Lomba <andersonlomba172@gmail.com>
2020-08-03 20:52:12 -0300

    Ecoleta, sua coleta de resíduos

---
+++ public/scripts/intro.js
@@ -0,0 +1,57 @@
+// document.write("Hello")
+// Variaveis, tipos de dados
+
+// let nome = "José "
+
+// document.write(nome)
+
+// const sobrenome = "Da Silva"
+
+// document.write(nome + sobrenome)
+
+// let nome = "Anderson "
+// let sobrenome = "Lomba de Oliveira"
+// document.write(nome+sobrenome)
+
+// const pessoa = {
+//   altura:"1,80",
+//   idade:24,
+//   solteiro: true
+// }
+// document.write(pessoa.solteiro)
+
+// Registrar função
+// function sayMyName() {
+//   document.write("Anderson") 
+// }
+
+// Executar função
+// sayMyName()
+
+// Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// }
+// sayMyName("Filipe ")
+// sayMyName("Gosta muito  ")
+// sayMyName(" de jogar  ")
+// sayMyName(" Free fire")
+
+// const notaFinal = 7
+// if(true) {
+//   //caminho 1
+// } else {
+//   //caminho 2
+// }
+
+// const notaFinal = 7
+
+// if(notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// for(i=0; i<10;i++) {
+//   document.write(`<p> ${i} </p>`)
+// }
